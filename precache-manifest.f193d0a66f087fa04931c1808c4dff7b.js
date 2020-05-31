ab2d74bf3332d15af13ab530d13bc236051ce5a7
Ba Nguyen <b.pynguyen@gmail.com>
2020-05-30 23:27:08 -0400

    Updates

---
+++ precache-manifest.f193d0a66f087fa04931c1808c4dff7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6bc2c5133b46dec27b2f25878757e69",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6b66535bd15cb3f0e1be",
+    "url": "/monster-rolodex/static/css/main.aa762f14.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monster-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b66535bd15cb3f0e1be",
+    "url": "/monster-rolodex/static/js/main.0e8e9952.chunk.js"
+  },
+  {
+    "revision": "b46281a00ff77db46658",
+    "url": "/monster-rolodex/static/js/runtime-main.c3b95803.js"
+  }
+]);
